01df14be1edd505a21dbd60905bc0619885e21bc
kangheeyoun <kkhy0304@naver.com>
2025-01-24 12:39:18 +0900

    update

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={
           <div className="app">
